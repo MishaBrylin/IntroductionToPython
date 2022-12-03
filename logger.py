b86bf52159fcfab83ba779fec0980bb64da60660
Misha <79090171508@yandex.ru>
2022-12-03 12:52:29 +0500

    main

--- logger.py
+++
@@ -1,7 +0,0 @@
-from datetime import datetime as dt
-
-
-def logger(action):
-    time = dt.now().strftime('%m/%d/%Y, %H:%M:%S')
-    with open('log.txt', 'a') as file:
-        file.write(f' {action} - {time}\n')
