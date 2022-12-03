42bec8361acbcaf0e7fe080963bb555f33a882eb
Misha <79090171508@yandex.ru>
2022-12-03 12:28:34 +0500

    Task7 -correct

--- model.py
+++ model.py
@@ -1,19 +1,18 @@
-
 import view
 # import controller as con
-
-
+ 
+ 
 def add_contact(list):
     list2 = view.get_value()
     if int(len(list)) == 0:
         list2[0] = int(len(list))+1
     else:
         list2[0] = list[int(len(list))-1][0]+1
-
+ 
     list.append(list2)
     return list
-
-
+ 
+ 
 def del_contact(list):
     x = view.menu3()
     i = 0
@@ -25,32 +24,32 @@ def del_contact(list):
     else:
         list.pop(i)
     return list
-
-
+ 
+ 
 def to_name_id(list):
     i = 0
     while i < int(len(list)):
         list[i][0] = i+1
         i += 1
     return list
-
-
+ 
+ 
 def sort_id(list):
     for i in range(int(len(list))):
         for j in range(int(len(list))):
             if list[i][0] < list[j][0]:
                 list[i], list[j] = list[j], list[i]
     return list
-
-
+ 
+ 
 def add_imp_contact(list):
     file_name = view.menu4()
     with open(file_name, 'r') as f:
         for line in f:
             list.append(line.split())
         return list
-
-
+ 
+ 
 def imp_contact(list):
     file_name = view.menu4()
     with open(file_name, 'r') as f:
@@ -59,8 +58,8 @@ def imp_contact(list):
             line = line[:-2]
             list.append(line.split(';'))
         return list
-
-
+ 
+ 
 def exp_contact(list):
     file_name = view.menu4()
     with open(file_name, 'w') as f:
@@ -71,8 +70,8 @@ def exp_contact(list):
             f.write('\n')
             i += 1
     return list
-
-
+ 
+ 
 def find_all_contact(list):
     x = view.menu5()
     z = not True
@@ -84,8 +83,8 @@ def find_all_contact(list):
                 break
             elif i == int(len(list))-1 and j == int(len(list[i]))-1 and z != True:
                 print('Поиск нечего не нашел')
-
-
+ 
+ 
 def find_surname(list):
     x = view.menu5()
     z = not True
--- view.py
+++ view.py
@@ -1,8 +1,6 @@
-
-
 from prettytable import PrettyTable
-
-
+ 
+ 
 def menu():
     print('''Для работы с телефонным справочником введите число:
     Просмотр всех записей справочника: - 1
@@ -11,7 +9,7 @@ def menu():
     Удаление записи из справочника по id: - 4
     Создать новый справочник импортировав справочник из фаила: - 5
     Добавить контакты в уже имеющий справочник из файла: - 6
-    Экспортировать справочник в файл: - 7 
+    Экспортировать справочник в файл: - 7
     Присвоить новые id попорядку: - 8
     Сортировать попорядку по id: - 9
     Найти в справочнике (поиск происходит по любой записи) - 10
@@ -21,31 +19,31 @@ def menu():
     while not 0 < n < 13:
         n = int(input('Введите номер из меню от 1 до 12: '))
     return n
-
-
+ 
+ 
 def menu2():
     n = int(input('Чтобы вернуться в меню нажмите 0 '))
     while not n == 0:
         n = int(input('Введите 0: '))
-
-
+ 
+ 
 def menu3():
     x = input('Введите номер id справочника, чтбы удалить запись: ')
-
+ 
     return x
-
-
+ 
+ 
 def menu4():
     x = input('Укажите название фаила: ')
-
+ 
     return x
-
-
+ 
+ 
 def menu5():
     x = input('Укажите цель поиска: ')
     return x
-
-
+ 
+ 
 def get_value():
     lst = [1, ]
     x = input('Введите имя: ')
@@ -59,8 +57,8 @@ def get_value():
     x = input('Введите номер телефона: ')
     lst.append(x)
     return lst
-
-
+ 
+ 
 def show_contact(list):
     i = 0
     while i < int(len(list)):
@@ -69,13 +67,13 @@ def show_contact(list):
         i += 1
         print()
         print()
-
-
+ 
+ 
 def show_contact2(list):
     i = 0
     new_Table = PrettyTable(
         ["id", "имя", "фамилия", "день рождения", "место работы", "номер телефона"])
-
+ 
     while i < int(len(list)):
         new_Table.add_row([list[i][0], list[i][1], list[i][2],
                            list[i][3], list[i][4], list[i][5], ])
